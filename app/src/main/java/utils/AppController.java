6252613f48ae15aaab41ed5f389534f210ef6cac
bunny <gamefever20@gmal.com>
2017-10-09 12:43:36 +0530

    added tabbed view for different feeds

--- app/src/main/java/utils/AppController.java
+++ app/src/main/java/utils/AppController.java
@@ -46,6 +46,7 @@ public class AppController extends Application {
 
 
 
+
     public <T> void addToRequestQueue(Request<T> req, String tag) {
         // set the default tag if tag is empty
         req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
