8972c3e710a17a7546fd289af7204427e9c4e2c8
bunny <gamefever20@gmal.com>
2018-01-20 02:29:12 +0530

    update 1.14

--- app/src/main/java/pib/affairs/current/app/pib/RssFeedFragment.java
+++ app/src/main/java/pib/affairs/current/app/pib/RssFeedFragment.java
@@ -478,6 +478,7 @@ public class RssFeedFragment extends Fragment {
                 SqlDatabaseHelper sqlDatabaseHelper = new SqlDatabaseHelper(getContext());
 
 
+
                 sqlDatabaseHelper.addSavedNews(news, response);
 
 
